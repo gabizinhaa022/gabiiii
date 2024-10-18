3bc795a637a6fc211b86cff4dea98d90cbb2788e
gabizinhaa022 <161498721+gabizinhaa022@users.noreply.github.com>
2024-10-18 14:54:47 -0300

    rtrtrg

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+ criaCartao(
+    "geografia"
+    "qual é o maior pais do mundo"
+    ruíssa
+ )
+ criaCartao(
+
+ ) 
+     "mateḿatica" (
+   "qual é a raiz quadrada de 144"
+   "12"
+
+)
+ criaCartao(
+     "lingua inglesa"
+     "como que fala escola em inglês"
+     "school"
+ )
+ criaCartao(
+    "séries"
+    'qual é sua séries favorita'
+    "outer banks"
+ )
