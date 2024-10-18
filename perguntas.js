effad676f5bd77724900980f076e29028784dee4
gabizinhaa022 <161498721+gabizinhaa022@users.noreply.github.com>
2024-10-18 15:10:54 -0300

    ff

--- perguntas.js
+++ perguntas.js
@@ -1,23 +1,25 @@
  criaCartao(
-    "geografia"
-    "qual é o maior pais do mundo"
-    ruíssa
+    "geografia",
+    "qual é o maior pais do mundo",
+    
+    'ruíssa'
  )
  criaCartao(
+    "mateḿatica", 
+        "qual é a raiz quadrada de 144",
+        "12"
+     
 
  ) 
-     "mateḿatica" (
-   "qual é a raiz quadrada de 144"
-   "12"
-
+   
 )
  criaCartao(
-     "lingua inglesa"
-     "como que fala escola em inglês"
+     "lingua inglesa",
+     "como que fala escola em inglês",
      "school"
  )
  criaCartao(
-    "séries"
-    'qual é sua séries favorita'
+    "séries",
+    'qual é sua séries favorita',
     "outer banks"
  )
